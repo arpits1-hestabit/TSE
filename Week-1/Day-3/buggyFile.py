arpits1-hestabit/TSE

commit feaa45cf1c689054e4e9b167644ef4ba89cd46bd

Introduced bug in this commit

Week-1/Day-3/buggyFile.py
@@ -1,0 +1,1 @@
+Buggy file with no changes

commit 752013458051349001765ddddafb56c55347437f

Fixed bug introduced in commit feaa45c

Week-1/Day-3/buggyFile.py
@@ -1,1 +1,2 @@
 Buggy file with no changes
+Changes the commit after git bisect