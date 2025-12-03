661deb66783a7a457c86bdc06cf5cf373463fc13
Arpit Saxena <arpits1.hestabit@gmail.com>
2025-12-03 23:49:33 +0530

    add user validation schema

---
+++ Week-4/prod/src/validation/user.schema.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+
+export const createUserSchema = Joi.object({
+    firstName: Joi.string().min(2).required(),
+    lastName: Joi.string().min(2).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required()
+});
