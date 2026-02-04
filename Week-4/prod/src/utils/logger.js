7d1d16508f34b57daa4f8f0cbf5aad28871becba
Arpit Saxena <the.arpit.saxena@gmail.com>
2026-02-04 21:54:13 +0530

    updated logger

--- Week-4/prod/src/utils/logger.js
+++ Week-4/prod/src/utils/logger.js
@@ -10,8 +10,8 @@ const logger = winston.createLogger({
     ),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'src/logs/app.log' }),// if I don't want to hardcode the paths, then I will have to make use of fileURLToPath and path modules.
-        new winston.transports.File({ filename: 'src/logs/error.log', level: 'error' })
+        new winston.transports.File({ filename: 'logs/app.log' }),
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' })
     ]
 });
 
