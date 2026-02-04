9d9739a556ea0e99d04a4222cef17e08d39a4283
Arpit Saxena <the.arpit.saxena@gmail.com>
2026-02-04 21:50:01 +0530

    adde env.js to add env

--- Week-4/prod/src/config/env.js
+++ Week-4/prod/src/config/env.js
@@ -2,20 +2,29 @@ import fs from "fs";
 import path from "path";
 import dotenv from "dotenv";
 
-const env = process.NODE_ENV || "local";  // for local|dev |prod
-const envFile = `.env.${env}`;
+const env = process.env.NODE_ENV || "local";
+
+// Map of environment file names
+const envFileMap = {
+  production: ".env.prod",
+  development: ".env.dev",
+  local: ".env.local",
+};
+
+const envFile = envFileMap[env] || `.env.${env}`;
 const envPath = path.resolve(process.cwd(), envFile);
 
 //will only execute if the file exists
 if (fs.existsSync(envPath)) {
-    dotenv.config({ path: envPath });
-    console.log(`[CONFIG] Loaded environment file:${envFile}`);
+  dotenv.config({ path: envPath });
+  console.log(`[CONFIG] Loaded environment file: ${envFile}`);
 } else {
-    console.warn(`[CONFIG] Environment file not found: ${envFile}`);
+  console.warn(`[CONFIG] Environment file not found: ${envFile}`);
+  process.exit(1);
 }
 
 export default {
-    port: process.env.PORT || 5000,
-    dbUri: process.env.DB_URI,
-    nodeEnv: env,
-}
+  port: process.env.PORT || 5000,
+  dbUri: process.env.MONGODB_URI || process.env.DB_URI,
+  nodeEnv: env,
+};
